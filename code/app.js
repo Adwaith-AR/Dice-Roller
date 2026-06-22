831e9872dcb27cf3a0f043d077fb4353ddecc4bc
Adwaith-AR <adwaith.rakhy.office@gamil.com>
2026-06-22 07:56:21 +0530

    bugs and maked it more clearner

--- code/app.js
+++ code/app.js
@@ -1,34 +1,46 @@
 const RollHistoryContainer = document.getElementById("rollHistory");
 const RollHistory = [];
-RollHistory.push(`<div id="rollhead"><h3 class="theme">Roll History</h3><h4 onclick="clearHistory()">Clear history</h4></div>`)
 const d4Btn = document.getElementById("d4Btn");
 const d6Btn = document.getElementById("d6Btn");;
 const d8Btn = document.getElementById("d8Btn");;
 const d10Btn = document.getElementById("d10Btn");;
+const rollbutton = document.getElementById("rollbutton");;
 const logoContainer = document.getElementById("logoContainer");;
 const themeContainer = document.getElementById("themeContainer");;
 
+let d4BtnS = false;
+let d6BtnS = true;
+let d8BtnS = false;
+let d10BtnS = false;
+let theme = "light"
+
+d6Btn.classList.add("activeBtn");
+RollHistory.push(`<div id="rollhead"><h3 class="theme">Roll History</h3><h4 onclick="clearHistory()">Clear history</h4></div>`)
+rollbutton.classList.add("dark");
 
 
-
-function themeDark(){
+function themeDark() {
+          rollbutton.innerHTML = ` <img src="./img/dice icon  dark.webp" alt="">RollDice`
+          rollbutton.classList.remove("dark");
           const text = document.querySelectorAll(".theme");
           const bg = document.querySelectorAll(".themeBg");
           const button = document.querySelectorAll(".button");
           text.forEach(element => {
                     element.classList.add("dark");
-
+                    
           });
           bg.forEach(element => {
                     element.classList.add("dark_bg");
-
+                    
           });
           button.forEach(element => {
                     element.classList.add("darkBtn");
-
+                    
           });
 }
 function themeLight() {
+          rollbutton.innerHTML = ` <img src="./img/dice icon.webp" alt="">RollDice`
+          rollbutton.classList.add("dark");
           const text = document.querySelectorAll(".theme");
           const bg = document.querySelectorAll(".themeBg");
           const button = document.querySelectorAll(".button");
@@ -46,19 +58,12 @@ function themeLight() {
           });
 }
 
-d6Btn.classList.add("activeBtn");
-
-let d4BtnS = false;
-let d6BtnS = true;
-let d8BtnS = false;
-let d10BtnS = false;
-let theme = "light"
 
 
-function switchTheme(){
-          if (theme=="light"){
+function switchTheme() {
+          if (theme == "light") {
                     themeDark()
-                    theme="dark";
+                    theme = "dark";
                     logoContainer.innerHTML = `<img src="./img/logo-dark.svg" alt="">`
                     themeContainer.innerHTML = `<img src="./img/moon.png" alt="">`
           }
@@ -130,23 +135,49 @@ function rollDice() {
           const img = [];
           const indexofhead = RollHistory.indexOf(`<div id="rollhead"><h3 class="theme">Roll History</h3><h4 onclick="clearHistory()">Clear history</h4></div>`)
           let total = 0;
+          let dice = 0;
 
-          for (let i = 0; i < NumOfDice; i++) {
-                    let value = Math.floor(Math.random() * 6) + 1;
-                    values.push(value);
-                    total = total + value;
-                    img.push(`<img src="./img/d6/6d${value}.webp" alt="">`);
-
-
-
-
+          if (d4BtnS) {
+                    dice = 4
+                    for (let i = 0; i < NumOfDice; i++) {
+                              let value = Math.floor(Math.random() * 4) + 1;
+                              values.push(value);
+                              total = total + value;
+                              img.push(`<img src="./img/d4/4d${value}.webp" alt="">`);
+                    }
           }
+          if (d6BtnS) {
+                    dice = 6
+                    for (let i = 0; i < NumOfDice; i++) {
+                              let value = Math.floor(Math.random() * 6) + 1;
+                              values.push(value);
+                              total = total + value;
+                              img.push(`<img src="./img/d6/6d${value}.webp" alt="">`);
+                    }
+          } if (d8BtnS) {
+                    dice = 8
+                    for (let i = 0; i < NumOfDice; i++) {
+                              let value = Math.floor(Math.random() * 8) + 1;
+                              values.push(value);
+                              total = total + value;
+                              img.push(`<img src="./img/d8/8d${value}.webp" alt="">`);
+                    }
+          } if (d10BtnS) {
+                    dice = 10
+                    for (let i = 0; i < NumOfDice; i++) {
+                              let value = Math.floor(Math.random() * 10) + 1;
+                              values.push(value);
+                              total = total + value;
+                              img.push(`<img src="./img/d10/10d${value}.webp" alt="">`);
+                    }
+          }
+
           RollHistory.splice(indexofhead, 1)
-          RollHistory.unshift(`<div><p class="theme">${NumOfDice}d6</p><p class="theme">(${values.join(",")})</p><p class="theme">${total}</p></div>`)
+          RollHistory.unshift(`<div><p class="theme">${NumOfDice}d${dice}</p><p class="theme">(${values.join(",")})</p><p class="theme">${total}</p></div>`)
           RollHistory.unshift(`<div id="rollhead"><h3 class="theme">Roll History</h3><h4 onclick="clearHistory()">Clear history</h4></div>`)
 
           RollHistoryContainer.innerHTML = RollHistory.join("");
-          outputNumContainer.innerHTML = ` <h2 class="theme" >${values.join(",") }</h2>`;
+          outputNumContainer.innerHTML = ` <h2 class="theme" >${values.join(",")}</h2>`;
           DiceImg.innerHTML = img.join("");
           if (theme == "dark") {
                     themeDark()
@@ -168,7 +199,7 @@ function clearHistory() {
           RollHistory.length = 0;
           RollHistory.unshift(`<div id="rollhead"><h3 class="theme">Roll History</h3><h4 onclick="clearHistory()">Clear history</h4></div>`)
           RollHistoryContainer.innerHTML = RollHistory.join("");
-          outputNumContainer.innerHTML = ` <h2 class="theme" ></h2>`;
+          outputNumContainer.innerHTML = ` <h2 class="theme" >output</h2>`;
           DiceImg.innerHTML = `<img src="./img/white dice.png" alt=""><img src="./img/orange dice.png" alt="">`;
           if (theme == "dark") {
                     themeDark()
